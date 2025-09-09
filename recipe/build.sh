d4b11a0612e5dc19069419f6989cad99d030e2cb
Pablo Winant <pablo.winant@gmail.com>
2025-09-09 17:43:31 +0200

    Update build script for Darwin platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ export BOOST_ROOT=$PREFIX
 
 if [ "$(uname)" == "Darwin" ]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  CXX="g++"
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
