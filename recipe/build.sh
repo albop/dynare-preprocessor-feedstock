61780f67404a394e8bbeab4746d2445196b34b3a
Pablo Winant <pablo.winant@gmail.com>
2025-09-09 13:13:04 +0200

    Enhance build.sh with macOS specific configuration
    Added conditional setup for macOS in build script.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 export BOOST_ROOT=$PREFIX
-meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread -nostdinc++ -nostdlib++'
+if [ "$(uname)" == "Darwin" ]; then
+  meson setup --buildtype=release build_preproc -Dcpp_args='-D_LIBCPP_DISABLE_AVAILABILITY'
+elif
+  meson setup --buildtype=release build_preproc
+fi
 meson compile -C build_preproc
 mkdir -p $PREFIX/bin
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
