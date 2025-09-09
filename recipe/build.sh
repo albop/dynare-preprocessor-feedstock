319841fad357e3f780405a56d2320f52f3d87b2e
Pablo Winant <pablo.winant@gmail.com>
2025-09-09 13:30:40 +0200

    Add pthread link argument for meson setup

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 
 export BOOST_ROOT=$PREFIX
 if [ "$(uname)" == "Darwin" ]; then
-  meson setup --buildtype=release build_preproc -Dcpp_args='-D_LIBCPP_DISABLE_AVAILABILITY'
+  meson setup --buildtype=release build_preproc -Dcpp_args='-D_LIBCPP_DISABLE_AVAILABILITY' -Dcpp_link_args='-pthread'
 else
-  meson setup --buildtype=release build_preproc
+  meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread'
 fi
 meson compile -C build_preproc
 mkdir -p $PREFIX/bin
